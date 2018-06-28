9d2c8ce639d4634abbfcedbb926f803c6e4c2264
doaa-altarawy <doaa.altarawy@gmail.com>
2018-06-28 14:25:58 -0400

    Add mean module

---
+++ RTP_python_template/mean.py
@@ -0,0 +1,15 @@
+
+def mean(num_list):
+    """
+    Claculate the mean of a sequence
+
+    Parameters
+    ----------
+    num_list: sequence of int or float
+
+    Returns
+    -------
+        mean of the sequence
+    """
+
+    return sum(num_list)/len(num_list)
