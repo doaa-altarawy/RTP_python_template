d4d2994c66694cdf0e91f95d6b82a3778fcbe281
doaa-altarawy <doaa.altarawy@gmail.com>
2018-06-29 14:12:26 -0400

    Add a split method

--- RTP_python_template/mean.py
+++ RTP_python_template/mean.py
@@ -13,3 +13,14 @@ def mean(num_list):
     """
 
     return sum(num_list)/len(num_list)
+
+
+def split(num_list, index):
+    """
+    Split a sequence into two from the given index
+    """
+
+    list1 = num_list[:index]
+    list2 = num_list[index:]
+
+    return list1, list2
